b342e6419a5daec67770a4a4754209ce2ec29a9e
yhirokane17 <131893435+yhirokane17@users.noreply.github.com>
2023-05-27 16:33:10 +0900

    投稿画面　商品説明まで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,17 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all
   end
 
-  def destroy
+  def new
+    @items = Item.all
   end
+
+  def create
+    Item create(item_params)
 end
+
+private
+def item_params
+  params.require(:item).permit(:item,:explanation,:category_id,:status_id,:delivery_id,:address_prefecture_id,:delivery_date_id,:price)
---
+++ db/migrate/20230527054435_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.text :item,null: false
+      t.text :explanation,null: false
+      t.integer :category_id,null: false
+      t.integer :status_id,null: false
+      t.integer :delivery_price_id,null: false
+      t.integer :address_prefecture_id,null: false
+      t.integer :delivery_date_id,null: false
+      t.integer :price,null: false
+       t.references :user,null:false,foreign_key:true
+      t.timestamps
+    end
+  end
+end
