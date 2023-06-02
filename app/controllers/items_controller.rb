6d9e8a879292f854d2feacb8588f7ff491b8f382
yhirokane17 <131893435+yhirokane17@users.noreply.github.com>
2023-06-02 18:06:02 +0900

    ログインまたは未ログイン時に直接編集urlを打ってもトップページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create,]
+  before_action :authenticate_user!, only: [:new, :create,:edit,:update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -25,7 +25,19 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
   end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+  end
+end
 
  private
   def item_params
