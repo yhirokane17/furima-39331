07da3804675a5a9c277e485fa52f675123b61e11
yhirokane17 <131893435+yhirokane17@users.noreply.github.com>
2023-06-30 05:26:50 +0900

    unicorn修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,25 @@ lock "~> 3.17.3"
 set :application, "hirokane"
 set :repo_url, "git@github.com:yhirokane17/furima-39331.git"
 
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.7.7'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/HIROKANE.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
